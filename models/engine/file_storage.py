3f0f338dd9a37b6940b4dc27b771b8b0a1cdc43a
PeteJames21 <peetjames21@gmail.com>
2023-10-11 21:42:07 +0300

    Implement the FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,28 +1,63 @@
 #!/usr/bin/python3
 """
-Defines a class serializes instances to a JSON file and deserializing
+Defines a class that serializes instances to a JSON file and deserializing
 a JSON file to instances.
 """
+
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
     """
     Serializes and deserializes instances using JSON
     """
-    __file_path = ...
-    __objects = ...
-
-    def __init__(self):
-        pass
+    __file_path = "db.json"
+    __objects = {}
 
     def all(self):
-        raise NotImplementedError
+        """Return a dict with all saved instances."""
+        return self.__class__.__objects
 
     def new(self, obj):
-        raise NotImplementedError
+        """Set in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        __objects[key] = obj
 
     def save(self):
-        raise NotImplementedError
+        """Serialize all objects in `__objects` to a JSON file."""
+        objs = {k: v.to_dict() for k, v in self.__class__.__objects.items()}
+        with open(self.__class__.__file_path, "w", encoding="utf-8") as f:
+            json.dump(objs, f)
 
     def reload(self):
-        raise NotImplementedError
+        """Update the list of objects using the JSON file, if present."""
+        valid_classes = {
+                            'Amenity', 'BaseModel', 'City', 'Place',
+                            'Review', 'State', 'User'
+                        }
+        try:
+            with open(self.__class__.__file_path, "r", encoding="utf-8") as f:
+                d = json.load(f)
+
+            for obj_dict in d.values():
+                # Get the class
+                cls_name = obj_dict["__class__"]
+                if cls_name not in valid_classes:
+                    continue
+                cls = eval(cls_name)
+
+                # Recreate the instance using its dict representation
+                del obj_dict["__class__"]  # This key should not be used
+                obj = cls(**obj_dict)
+                # Save the instance to __objects
+                self.new(obj)
+
+        except FileNotFoundError:
+            pass
--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from base_model import BaseModel
 
 
 class User(BaseModel):
+    """Models an AirBnB user."""
     email = ""
     password = ""
     first_name = ""
