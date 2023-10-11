77203244f16d39861f0f4c833b4864a58d4a14a6
Gabogogi <bencitymaina@gmail.com>
2023-10-11 21:46:19 +0300

    created base_model.py: tasks 3 and 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,49 @@
 #!/usr/bin/python3
-"""
-Define a BaseModel class that defines all common attributes/methods
-for other classes.
-"""
+
+from datetime import datetime
+import uuid
 
 
 class BaseModel:
-    """A base class for all other classes."""
-
+    '''Defines all common attributes/methods for other classes'''
     def __init__(self, *args, **kwargs):
-        self.id = ...
-        self.created_at = ...
-        self.updated_at = ...
+        '''
+        Assigns each instance with a unique id
+        
+        Assigns current time and updated time
+        re-creates an instance with the dictionary representation
+        '''
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ('created_at', 'updated_at'):
+                    self.__dict__[key] = datetime.strptime(value, date_format)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
-        raise NotImplementedError
+        '''updates the public instance attribute updated_at
+            with the current datetime'''
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        raise NotImplementedError
+        '''
+        creates and returns a dictionary representation with
+        “simple object type” of our BaseModel
+        a key __class__ contains the class name of the object
+        '''
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        return obj_dict
 
     def __str__(self):
-        raise NotImplementedError
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                self.__dict__))
